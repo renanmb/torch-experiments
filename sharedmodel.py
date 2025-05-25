ad13d4bcfab0e5592f6dfa14a5e4cdb653ed3374
Renan <renanm.b3@gmail.com>
2025-05-25 16:34:02 -0500

    first

---
+++ sharedmodel.py
@@ -0,0 +1,59 @@
+
+# This is what the model instantiator is making in the SKRL
+
+#   logger.warn(
+# [skrl:INFO] Environment wrapper: Isaac Lab (single-agent)
+# [skrl:INFO] Seed: 42
+# ==================================================
+# Shared model (roles): ['policy', 'value']
+# ==================================================
+
+class SharedModel(GaussianMixin,DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(
+            self,
+            clip_actions=False,
+            clip_log_std=True,
+            min_log_std=-20.0,
+            max_log_std=2.0,
+            reduction="sum",
+            role="policy",
+        )
+        DeterministicMixin.__init__(self, clip_actions=False, role="value")
+
+        self.net_container = nn.Sequential(
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+        )
+        self.policy_layer = nn.LazyLinear(out_features=self.num_actions)
+        self.log_std_parameter = nn.Parameter(torch.full(size=(self.num_actions,), fill_value=0.0), requires_grad=True)
+        self.value_layer = nn.LazyLinear(out_features=1)
+
+    def act(self, inputs, role):
+        if role == "policy":
+            return GaussianMixin.act(self, inputs, role)
+        elif role == "value":
+            return DeterministicMixin.act(self, inputs, role)
+    
+    def compute(self, inputs, role=""):
+        if role == "policy":
+            states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+            taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+            net = self.net_container(states)
+            self._shared_output = net
+            output = self.policy_layer(net)
+            return output, self.log_std_parameter, {}
+        elif role == "value":
+            if self._shared_output is None:
+                states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+                taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+                net = self.net_container(states)
+                shared_output = net
+            else:
+                shared_output = self._shared_output
+            self._shared_output = None
+            output = self.value_layer(shared_output)
+            return output, {}
