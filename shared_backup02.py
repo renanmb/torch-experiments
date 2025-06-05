e2674d4bfdb0bd5ed40854caa7e695fc6171f6ba
Renan <renanm.b3@gmail.com>
2025-06-05 02:33:36 -0500

    almost to the end

---
+++ shared_backup02.py
@@ -0,0 +1,300 @@
+from typing import Any, Mapping, Optional, Sequence, Tuple, Union
+
+import textwrap
+import gymnasium
+
+import torch
+import torch.nn as nn  # noqa
+
+from skrl.models.torch import Model  # noqa
+from skrl.models.torch import (  # noqa
+    CategoricalMixin,
+    DeterministicMixin,
+    GaussianMixin,
+    MultiCategoricalMixin,
+    MultivariateGaussianMixin,
+)
+from skrl.utils.model_instantiators.torch.common import one_hot_encoding  # noqa
+from skrl.utils.model_instantiators.torch.common import convert_deprecated_parameters, generate_containers
+from skrl.utils.spaces.torch import unflatten_tensorized_space  # noqa
+
+
+def shared_model(
+    observation_space: Optional[Union[int, Tuple[int], gymnasium.Space]] = None,
+    action_space: Optional[Union[int, Tuple[int], gymnasium.Space]] = None,
+    device: Optional[Union[str, torch.device]] = None,
+    structure: Sequence[str] = ["GaussianMixin", "DeterministicMixin"],
+    roles: Sequence[str] = [],
+    parameters: Sequence[Mapping[str, Any]] = [],
+    single_forward_pass: bool = True,
+    return_source: bool = False,
+) -> Union[Model, str]:
+    """Instantiate a shared model
+
+    :param observation_space: Observation/state space or shape (default: None).
+                              If it is not None, the num_observations property will contain the size of that space
+    :type observation_space: int, tuple or list of integers, gymnasium.Space or None, optional
+    :param action_space: Action space or shape (default: None).
+                         If it is not None, the num_actions property will contain the size of that space
+    :type action_space: int, tuple or list of integers, gymnasium.Space or None, optional
+    :param device: Device on which a tensor/array is or will be allocated (default: ``None``).
+                   If None, the device will be either ``"cuda"`` if available or ``"cpu"``
+    :type device: str or torch.device, optional
+    :param structure: Shared model structure (default: Gaussian-Deterministic).
+    :type structure: sequence of strings, optional
+    :param roles: Organized list of model roles (default: ``[]``)
+    :type roles: sequence of strings, optional
+    :param parameters: Organized list of model instantiator parameters (default: ``[]``)
+    :type parameters: sequence of dict, optional
+    :param single_forward_pass: Whether to perform a single forward-pass for the shared layers/network (default: ``True``)
+    :type single_forward_pass: bool
+    :param return_source: Whether to return the source string containing the model class used to
+                          instantiate the model rather than the model instance (default: False).
+    :type return_source: bool, optional
+
+    :return: Shared model instance or definition source
+    :rtype: Model
+    """
+
+    def get_init(class_name, parameter, role):
+        if class_name.lower() == "categoricalmixin":
+            return f'CategoricalMixin.__init__(self, unnormalized_log_prob={parameter.get("unnormalized_log_prob", True)}, role="{role}")'
+        elif class_name.lower() == "multicategoricalmixin":
+            return f'MultiCategoricalMixin.__init__(self, unnormalized_log_prob={parameter.get("unnormalized_log_prob", True)}, reduction="{parameter.get("reduction", "sum")}", role="{role}")'
+        elif class_name.lower() == "deterministicmixin":
+            return (
+                f'DeterministicMixin.__init__(self, clip_actions={parameter.get("clip_actions", False)}, role="{role}")'
+            )
+        elif class_name.lower() == "gaussianmixin":
+            return f"""GaussianMixin.__init__(
+            self,
+            clip_actions={parameter.get("clip_actions", False)},
+            clip_log_std={parameter.get("clip_log_std", True)},
+            min_log_std={parameter.get("min_log_std", -20)},
+            max_log_std={parameter.get("max_log_std", 2)},
+            reduction="{parameter.get("reduction", "sum")}",
+            role="{role}",
+        )"""
+        elif class_name.lower() == "multivariategaussianmixin":
+            return f"""MultivariateGaussianMixin.__init__(
+            self,
+            clip_actions={parameter.get("clip_actions", False)},
+            clip_log_std={parameter.get("clip_log_std", True)},
+            min_log_std={parameter.get("min_log_std", -20)},
+            max_log_std={parameter.get("max_log_std", 2)},
+            role="{role}",
+        )"""
+        raise ValueError(f"Unknown class: {class_name}")
+
+    def get_return(class_name):
+        if class_name.lower() == "categoricalmixin":
+            return r"output, {}"
+        elif class_name.lower() == "multicategoricalmixin":
+            return r"output, {}"
+        elif class_name.lower() == "deterministicmixin":
+            return r"output, {}"
+        elif class_name.lower() == "gaussianmixin":
+            return r"output, self.log_std_parameter, {}"
+        elif class_name.lower() == "multivariategaussianmixin":
+            return r"output, self.log_std_parameter, {}"
+        raise ValueError(f"Unknown class: {class_name}")
+
+    def get_extra(class_name, parameter, role, model):
+        if class_name.lower() == "categoricalmixin":
+            return ""
+        elif class_name.lower() == "multicategoricalmixin":
+            return ""
+        elif class_name.lower() == "deterministicmixin":
+            return ""
+        elif class_name.lower() == "gaussianmixin":
+            initial_log_std = float(parameter.get("initial_log_std", 0))
+            fixed_log_std = parameter.get("fixed_log_std", False)
+            return f'self.log_std_parameter = nn.Parameter(torch.full(size=({model["output"]["size"]},), fill_value={initial_log_std}), requires_grad={not fixed_log_std})'
+        elif class_name.lower() == "multivariategaussianmixin":
+            initial_log_std = float(parameter.get("initial_log_std", 0))
+            fixed_log_std = parameter.get("fixed_log_std", False)
+            return f'self.log_std_parameter = nn.Parameter(torch.full(size=({model["output"]["size"]},), fill_value={initial_log_std}), requires_grad={not fixed_log_std})'
+        raise ValueError(f"Unknown class: {class_name}")
+
+    # compatibility with versions prior to 1.3.0
+    if not "network" in parameters[0]:
+        parameters[0]["network"], parameters[0]["output"] = convert_deprecated_parameters(parameters[0])
+        parameters[1]["network"], parameters[1]["output"] = convert_deprecated_parameters(parameters[1])
+        # delete deprecated parameters
+        for parameter in [
+            "input_shape",
+            "hiddens",
+            "hidden_activation",
+            "output_shape",
+            "output_activation",
+            "output_scale",
+        ]:
+            if parameter in parameters[0]:
+                del parameters[0][parameter]
+            if parameter in parameters[1]:
+                del parameters[1][parameter]
+
+    # checking
+    assert (
+        len(structure) == len(roles) == len(parameters)
+    ), f"Invalid configuration: structures ({len(structure)}), roles ({len(roles)}) and parameters ({len(parameters)}) have different lengths"
+
+    models = [{"class": item} for item in structure]
+
+    # parse model definitions
+    for i, model in enumerate(models):
+        model["forward"] = []
+        model["networks"] = []
+        model["hooks"] = [] # HOOKS - experiment
+        model["containers"], model["output"] = generate_containers(
+            parameters[i]["network"], parameters[i]["output"], embed_output=False, indent=1
+        )
+
+    # network definitions
+    networks_common = []
+    forward_common = []
+    # Hooks definitions
+    hook_common = []
+    for container in models[0]["containers"]:
+        networks_common.append(f'self.{container["name"]}_container = {container["sequential"]}')
+        forward_common.append(f'{container["name"]} = self.{container["name"]}_container({container["input"]})')
+        hook_common.append(f'x = self.{container["name"]}_container(x)') # {container["name"]}  {container["input"]}
+    forward_common.insert(
+        0, 'taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))'
+    )
+    forward_common.insert(0, 'states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))')
+
+    # process output
+    if models[0]["output"]["modules"]:
+        models[0]["networks"].append(f'self.{roles[0]}_layer = {models[0]["output"]["modules"][0]}')
+        models[0]["forward"].append(f'output = self.{roles[0]}_layer({container["name"]})')
+        models[0]["hooks"].append(f'x = self.{roles[0]}_layer(x)')
+    if models[0]["output"]["output"]:
+        models[0]["forward"].append(f'output = {models[0]["output"]["output"]}')
+    else:
+        models[0]["forward"][-1] = models[0]["forward"][-1].replace(f'{container["name"]} =', "output =", 1)
+
+    if models[1]["output"]["modules"]:
+        models[1]["networks"].append(f'self.{roles[1]}_layer = {models[1]["output"]["modules"][0]}')
+        models[1]["forward"].append(
+            f'output = self.{roles[1]}_layer({"shared_output" if single_forward_pass else container["name"]})'
+        )
+    if models[1]["output"]["output"]:
+        models[1]["forward"].append(f'output = {models[1]["output"]["output"]}')
+    else:
+        models[1]["forward"][-1] = models[1]["forward"][-1].replace(f'{container["name"]} =', "output =", 1)
+
+    # build substitutions and indent content
+    networks_common = textwrap.indent("\n".join(networks_common), prefix=" " * 8)[8:]
+    hook_common = textwrap.indent("\n".join(hook_common), prefix=" " * 8)[8:] # Hooks
+    models[0]["networks"] = textwrap.indent("\n".join(models[0]["networks"]), prefix=" " * 8)[8:]
+    extra = get_extra(structure[0], parameters[0], roles[0], models[0])
+    if extra:
+        models[0]["networks"] += "\n" + textwrap.indent(extra, prefix=" " * 8) # this fixes the indentation self.log_std_parameter 
+    models[1]["networks"] = textwrap.indent("\n".join(models[1]["networks"]), prefix=" " * 8)[8:]
+    extra = get_extra(structure[1], parameters[1], roles[1], models[1])
+    if extra:
+        models[1]["networks"] += "\n" + textwrap.indent(extra, prefix=" " * 8) # this fixes the indentation self.log_std_parameter 
+
+    if single_forward_pass:
+        models[1]["forward"] = (
+            [
+                "if self._shared_output is None:",
+            ]
+            + ["    " + item for item in forward_common]
+            + [
+                f'    shared_output = {container["name"]}',
+                "else:",
+                "    shared_output = self._shared_output",
+                "self._shared_output = None",
+            ]
+            + models[1]["forward"]
+        )
+        forward_common.append(f'self._shared_output = {container["name"]}')
+        forward_common = textwrap.indent("\n".join(forward_common), prefix=" " * 12)[12:]
+        # hook_common = textwrap.indent("\n".join(forward_common), prefix=" " * 12)[12:] # hooks
+    else:
+        forward_common = textwrap.indent("\n".join(forward_common), prefix=" " * 8)[8:]
+        # hook_common = textwrap.indent("\n".join(forward_common), prefix=" " * 8)[8:] # hooks
+
+    models[0]["forward"] = textwrap.indent("\n".join(models[0]["forward"]), prefix=" " * 12)[12:]
+    models[0]["hooks"] = textwrap.indent("\n".join(models[0]["hooks"]), prefix=" " * 12)[12:]
+    models[1]["forward"] = textwrap.indent("\n".join(models[1]["forward"]), prefix=" " * 12)[12:]
+    
+    # region Torch Hook
+    # roles[0] seems to be the policy and roles[1] seems to be value
+    # I am using object states because dont know how to properly change -- def forward(self, states, role=""):
+    # we can use the nn.Sequential Directly e.g.:
+    # self._nn = nn.Sequential(self.net_container, self.policy_layer) ---- This gives error as it is not defined in the load_state_dict  
+
+    template = f"""class SharedModel({",".join(structure)}, Model):
+    def __init__(self, observation_space, action_space, device):
+        Model.__init__(self, observation_space, action_space, device)
+        {get_init(structure[0], parameters[0], roles[0])}
+        {get_init(structure[1], parameters[1], roles[1])}
+
+        {networks_common}
+        {models[0]["networks"]}
+        {models[1]["networks"]}
+
+    def act(self, inputs, role):
+        if role == "{roles[0]}":
+            return {structure[0]}.act(self, inputs, role)
+        elif role == "{roles[1]}":
+            return {structure[1]}.act(self, inputs, role)
+    """
+    if single_forward_pass:
+        template += f"""
+    def compute(self, inputs, role=""):
+        if role == "{roles[0]}":
+            {forward_common}
+            {models[0]["forward"]}
+            return {get_return(structure[0])}
+        elif role == "{roles[1]}":
+            {models[1]["forward"]}
+            return {get_return(structure[1])}
+    """
+    else:
+        template += f"""
+    def compute(self, inputs, role=""):
+        {forward_common}
+        if role == "{roles[0]}":
+            {models[0]["forward"]}
+            return {get_return(structure[0])}
+        elif role == "{roles[1]}":
+            {models[1]["forward"]}
+            return {get_return(structure[1])}
+    """
+
+    if single_forward_pass:
+        template += f"""
+    def forward(self, x):
+        {hook_common}
+        {models[0]["hooks"]}
+        return x
+    """
+    else:
+        template += f"""
+    def forward(self, x):
+        {hook_common}
+        {models[0]["hooks"]}
+        return x
+    """
+    # self._nn = MyModel(self)
+    # it should be x = self.net_container and x = self.policy_layer
+    # end region
+
+    # return source
+    if return_source:
+        return template
+
+    # instantiate model
+    _locals = {}
+    exec(template, globals(), _locals)
+    return _locals["SharedModel"](observation_space=observation_space, action_space=action_space, device=device)
+
+# def forward(self, x):
+#     for name, module in self._modules.items():
+#         print(f"Passing through {name}: {module}")
+#         x = module(x)
+#     return x
--- skrl-camera-ppo/sharedmodel.py
+++ skrl-camera-ppo/sharedmodel.py
@@ -75,3 +75,218 @@ class SharedModel(GaussianMixin,DeterministicMixin, Model):
             self._shared_output = None
             output = self.value_layer(shared_output)
             return output, {}
+
+# ==================================================
+# ==================================================
+# Trying to combine the Modules with the forward method
+# ==================================================
+# ==================================================
+        
+# logger.warn(
+# [skrl:INFO] Environment wrapper: Isaac Lab (single-agent)
+# [skrl:INFO] Seed: 42
+# ==================================================
+# Shared model (roles): ['policy', 'value']
+# ==================================================
+
+class SharedModel(GaussianMixin,DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(
+            self,
+            clip_actions=False,
+            clip_log_std=True,
+            min_log_std=-20.0,
+            max_log_std=2.0,
+            reduction="sum",
+            role="policy",
+        )
+        DeterministicMixin.__init__(self, clip_actions=False, role="value")
+
+        self.net_container = nn.Sequential(
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+        )
+        self.policy_layer = nn.LazyLinear(out_features=self.num_actions)
+        self.log_std_parameter = nn.Parameter(torch.full(size=(self.num_actions,), fill_value=0.0), requires_grad=True)
+        self.value_layer = nn.LazyLinear(out_features=1)
+
+    def act(self, inputs, role):
+        if role == "policy":
+            return GaussianMixin.act(self, inputs, role)
+        elif role == "value":
+            return DeterministicMixin.act(self, inputs, role)
+    
+    def compute(self, inputs, role=""):
+        if role == "policy":
+            states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+            taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+            net = self.net_container(states)
+            self._shared_output = net
+            output = self.policy_layer(net)
+            return output, self.log_std_parameter, {}
+        elif role == "value":
+            if self._shared_output is None:
+                states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+                taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+                net = self.net_container(states)
+                shared_output = net
+            else:
+                shared_output = self._shared_output
+            self._shared_output = None
+            output = self.value_layer(shared_output)
+            return output, {}
+    
+    def forward(self, x):
+        x = self.net_container(x)
+        x = self.policy_layer(x)
+        return x
+
+# ==================================================
+# ==================================================
+# Trying to combine the Modules with class MyModel(torch.nn.Module):
+# ==================================================
+# ==================================================
+# There is an issue with the indentation
+
+# logger.warn(
+# [skrl:INFO] Environment wrapper: Isaac Lab (single-agent)
+# [skrl:INFO] Seed: 42
+# ==================================================
+# Shared model (roles): ['policy', 'value']
+# ==================================================
+
+class SharedModel(GaussianMixin,DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(
+            self,
+            clip_actions=False,
+            clip_log_std=True,
+            min_log_std=-20.0,
+            max_log_std=2.0,
+            reduction="sum",
+            role="policy",
+        )
+        DeterministicMixin.__init__(self, clip_actions=False, role="value")
+
+        self.net_container = nn.Sequential(
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+        )
+        self.policy_layer = nn.LazyLinear(out_features=self.num_actions)
+        self.log_std_parameter = nn.Parameter(torch.full(size=(self.num_actions,), fill_value=0.0), requires_grad=True)
+        self.value_layer = nn.LazyLinear(out_features=1)
+        self._nn = MyModel(self)
+
+    def act(self, inputs, role):
+        if role == "policy":
+            return GaussianMixin.act(self, inputs, role)
+        elif role == "value":
+            return DeterministicMixin.act(self, inputs, role)
+    
+    def compute(self, inputs, role=""):
+        if role == "policy":
+            states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+            taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+            net = self.net_container(states)
+            self._shared_output = net
+            output = self.policy_layer(net)
+            return output, self.log_std_parameter, {}
+        elif role == "value":
+            if self._shared_output is None:
+                states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+                taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+                net = self.net_container(states)
+                shared_output = net
+            else:
+                shared_output = self._shared_output
+            self._shared_output = None
+            output = self.value_layer(shared_output)
+            return output, {}
+    class MyModel(torch.nn.Module):
+    def __init__(self, policy):
+        super(MyModel, self).__init__()
+        # Define the sequential part
+        self.sequential = policy.net_container 
+        # Define the linear part
+        self.linear = policy.policy_layer
+    
+    def forward(self, x):
+        x = self.net_container(x)
+        x = self.policy_layer(x)
+        return x
+    
+# logger.warn(
+# [skrl:INFO] Environment wrapper: Isaac Lab (single-agent)
+# [skrl:INFO] Seed: 42
+# ==================================================
+# Shared model (roles): ['policy', 'value']
+# ==================================================
+
+class SharedModel(GaussianMixin,DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(
+            self,
+            clip_actions=False,
+            clip_log_std=True,
+            min_log_std=-20.0,
+            max_log_std=2.0,
+            reduction="sum",
+            role="policy",
+        )
+        DeterministicMixin.__init__(self, clip_actions=False, role="value")
+
+        self.net_container = nn.Sequential(
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+            nn.LazyLinear(out_features=32),
+            nn.ELU(),
+        )
+        self.policy_layer = nn.LazyLinear(out_features=self.num_actions)
+        self.log_std_parameter = nn.Parameter(torch.full(size=(self.num_actions,), fill_value=0.0), requires_grad=True)
+        self.value_layer = nn.LazyLinear(out_features=1)
+        self._nn = MyModel(self)
+
+    def act(self, inputs, role):
+        if role == "policy":
+            return GaussianMixin.act(self, inputs, role)
+        elif role == "value":
+            return DeterministicMixin.act(self, inputs, role)
+    
+    def compute(self, inputs, role=""):
+        if role == "policy":
+            states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+            taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+            net = self.net_container(states)
+            self._shared_output = net
+            output = self.policy_layer(net)
+            return output, self.log_std_parameter, {}
+        elif role == "value":
+            if self._shared_output is None:
+                states = unflatten_tensorized_space(self.observation_space, inputs.get("states"))
+                taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
+                net = self.net_container(states)
+                shared_output = net
+            else:
+                shared_output = self._shared_output
+            self._shared_output = None
+            output = self.value_layer(shared_output)
+            return output, {}
+    class MyModel(torch.nn.Module):
+    def __init__(self, policy):
+        super(MyModel, self).__init__()
+        # Define the sequential part
+        self.sequential = policy.net_container 
+        # Define the linear part
+        self.linear = policy.policy_layer
+
+    def forward(self, x):
+        x = self.net_container(x)
+        x = self.policy_layer(x)
+        return x
